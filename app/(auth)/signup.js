c5202e3c50f9dc685ae9ba88c19599ec998e923b
Prodigy75000 <armand.bireaud@gmail.com>
2026-03-30 17:49:32 +0200

    fixed layout

--- app/(auth)/signup.js
+++ app/(auth)/signup.js
@@ -1,14 +1,8 @@
 import { useState } from "react";
-import { useAuth } from "../context/AuthContext";
-import {
-  StyleSheet,
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  Alert,
-} from "react-native";
+import { useAuth } from "../../context/AuthContext";
+import { View, Text, TextInput, TouchableOpacity, Alert } from "react-native";
 import { useRouter } from "expo-router";
+import styles from "../../styles/signupStyles";
 import axios from "axios";
 
 const API_URL = "https://site--gift-squad-back--r62dpvlsxwvq.code.run";
