a210f99b0b91911e7fd6e3c2a49d3c107dab5d05
Prodigy75000 <armand.bireaud@gmail.com>
2026-03-31 11:11:44 +0200

    changes auth to user in the front route call

--- app/(auth)/index.js
+++ app/(auth)/index.js
@@ -13,7 +13,7 @@ export default function LoginScreen() {
 
   const handleLogin = async () => {
     try {
-      const { data } = await api.post("/auth/login", {
+      const { data } = await api.post("/user/login", {
         email,
         password,
       });
--- app/(auth)/signup.js
+++ app/(auth)/signup.js
@@ -9,7 +9,7 @@ export default function SignupScreen() {
   const [form, setForm] = useState({
     firstname: "",
     lastname: "",
-    username: "",
+    pseudo: "",
     email: "",
     password: "",
     confirmPassword: "",
@@ -23,10 +23,10 @@ export default function SignupScreen() {
     }
 
     try {
-      const { data } = await api.post("/auth/signup", {
+      const { data } = await api.post("/user/signup", {
         firstname: form.firstname,
         lastname: form.lastname,
-        username: form.username,
+        pseudo: form.pseudo,
         email: form.email,
         password: form.password,
       });
@@ -59,8 +59,8 @@ export default function SignupScreen() {
       />
       <TextInput
         placeholder="Pseudo"
-        onChangeText={(v) => setForm({ ...form, username: v })}
-        value={form.username}
+        onChangeText={(v) => setForm({ ...form, pseudo: v })}
+        value={form.pseudo}
       />
       <TextInput
         placeholder="Email"
