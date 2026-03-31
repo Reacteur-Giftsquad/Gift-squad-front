c110830f4647bc05b1b8dc969c139b57d75a6535
Ewa Majowska <e.majowska@etik.com>
2026-03-31 14:08:09 +0200

    Fix: vertical align for buttons

--- components/SubmitButton.js
+++ components/SubmitButton.js
@@ -1,12 +1,13 @@
-import { Pressable, StyleSheet, Text } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import colors from "../assets/colors/colors.json";
 
 const SubmitButton = ({ icon, text, onPress }) => {
   return (
     <Pressable style={styles.button} onPress={onPress}>
-      <Text style={styles.text}>
-        {icon} {text}
-      </Text>
+      <View style={styles.buttonContent}>
+        <Text>{icon}</Text>
+        <Text style={styles.text}>{text}</Text>
+      </View>
     </Pressable>
   );
 };
@@ -16,15 +17,17 @@ const styles = StyleSheet.create({
     backgroundColor: colors.green,
     padding: 12,
     borderRadius: 5,
-    flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
+  },
+  buttonContent: {
+    alignItems: "center",
+    flexDirection: "row",
     gap: 10,
   },
   text: {
     fontSize: 18,
     color: "white",
-    textAlign: "center",
   },
 });
 export default SubmitButton;
