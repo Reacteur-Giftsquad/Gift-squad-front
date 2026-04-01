a1222798c73cc45de997d226182dbbd380fa9509
Prodigy75000 <armand.bireaud@gmail.com>
2026-04-01 12:49:03 +0200

    couple of fixed after ovehaul

--- app/(auth)/signup.js
+++ app/(auth)/signup.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useAuth } from "../../context/AuthContext";
 import { View, Alert } from "react-native";
 import { useRouter } from "expo-router";
-import styles from "../../styles/loginStyles";
+import styles from "../../styles/globals";
 import api from "../../utils/api";
 import Header from "../../components/Header";
 import Input from "../../components/Input";
