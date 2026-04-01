c3cb9e292834d60574cf6168c9babb116220a542
Ewa Majowska <em.majowska@gmail.com>
2026-04-01 13:36:27 +0400

    Fixed profil form

--- app/(main)/profil.js
+++ app/(main)/profil.js
@@ -11,42 +11,27 @@ import Input from "../../components/Input";
 import SubmitButton from "../../components/SubmitButton";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import ScreenWithMenu from "../../components/ScreenWithMenu";
 
 export default function Profil() {
-  const [isLoading, setIsLoading] = useState(true);
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [user, setUser] = useState(true);
-  const { userId, logout } = useAuth();
+  const { user, logout } = useAuth();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const { data } = api.get(`/user/${userId}`);
-        setUser(data);
-        setIsLoading(false);
-      } catch (error) {
-        if (error.response) {
-          Alert.alert(
-            "Erreur",
-            error.response.data.message ||
-              "Erreur lors de la récuperation de données",
-          );
-        } else {
-          Alert.alert("Erreur", "Impossible de se connecter au serveur");
-        }
-      }
-    };
-    fetchData();
-  }, []);
+  const [form, setForm] = useState({
+    firstname: user.firstname,
+    lastname: user.lastname,
+    pseudo: user.pseudo,
+    email: user.email,
+  });
 
   const handleSubmit = async () => {
     setIsSubmitting(true);
     try {
-      const { data } = await api.post(`/user/modify/${userId}`, {
-        firstname: user.firstname,
-        lastname: user.lastname,
-        pseudo: user.pseudo,
-        email: user.email,
+      const { data } = await api.post(`/user/modify/${user._id}`, {
+        firstname: form.firstname,
+        lastname: form.lastname,
+        pseudo: form.pseudo,
+        email: form.email,
       });
 
       setUser(data);
@@ -65,59 +50,61 @@ export default function Profil() {
   };
 
   return (
-    <View style={styles.content}>
-      {isLoading ? (
-        <ActivityIndicator color={colors.green} />
-      ) : (
-        <>
-          <View style={styles.center}>
-            <Octicons name="feed-person" size={100} color={colors.green} />
-            <Title text={user.firstname + " " + user.lastname} heading="h1" />
-            <LightText text={`@${user.pseudo}`} />
-          </View>
-          <View style={styles.gap}>
-            <Title text="INFORMATION PERSONNELLES" heading="h2" />
-            <Input
-              title="Prénom"
-              placeholder={"Votre prénom"}
-              value={user.firstname}
-              setState={(v) => setUser({ ...user, firstname: v })}
-            />
-            <Input
-              title="Nom"
-              placeholder={"Votre nom"}
-              value={user.lastname}
-              setState={(v) => setUser({ ...user, lastname: v })}
-            />
-            <Input
-              title="Pseudo"
-              placeholder="Choisissez un pseudo unique"
-              value={user.pseudo}
-              onChangeText={(v) => setUser({ ...user, pseudo: v })}
-            />
-            <Input
-              title="Email"
-              placeholder="Votre email"
-              setState={(v) => setUser({ ...user, email: v })}
-              value={user.email}
-            />
-          </View>
-          <View style={styles.gap}>
-            <SubmitButton
-              text="Enregistrer les modifications"
-              onPress={handleSubmit}
-              isSubmitting={isSubmitting}
-              icon={<Ionicons name="save" size={24} color="white" />}
-            />
-            <SubmitButton
-              text="Se déconnecterr"
-              onPress={logout}
-              bgColor={colors.red}
-              icon={<MaterialIcons name="logout" size={24} color="white" />}
-            />
-          </View>
-        </>
-      )}
-    </View>
+    <ScreenWithMenu title="Mon profil">
+      <View style={styles.content}>
+        {isLoading ? (
+          <ActivityIndicator />
+        ) : (
+          <>
+            <View style={styles.center}>
+              <Octicons name="feed-person" size={100} color={colors.green} />
+              <Title text={user.firstname + " " + user.lastname} heading="h1" />
+              <LightText text={`@${user.pseudo}`} />
+            </View>
+            <View style={styles.gap}>
+              <Title text="INFORMATION PERSONNELLES" heading="h2" />
+              <Input
+                title="Prénom"
+                placeholder={"Votre prénom"}
+                value={form.firstname}
+                setState={(v) => setForm({ ...form, firstname: v })}
+              />
+              <Input
+                title="Nom"
+                placeholder={"Votre nom"}
+                value={form.lastname}
+                setState={(v) => setForm({ ...form, lastname: v })}
+              />
+              <Input
+                title="Pseudo"
+                placeholder="Choisissez un pseudo unique"
+                value={form.pseudo}
+                onChangeText={(v) => setForm({ ...form, pseudo: v })}
+              />
+              <Input
+                title="Email"
+                placeholder="Votre email"
+                setState={(v) => setForm({ ...form, email: v })}
+                value={form.email}
+              />
+            </View>
+            <View style={styles.gap}>
+              <SubmitButton
+                text="Enregistrer les modifications"
+                onPress={handleSubmit}
+                isSubmitting={isSubmitting}
+                icon={<Ionicons name="save" size={24} color="white" />}
+              />
+              <SubmitButton
+                text="Se déconnecterr"
+                onPress={logout}
+                bgColor={colors.red}
+                icon={<MaterialIcons name="logout" size={24} color="white" />}
+              />
+            </View>
+          </>
+        )}
+      </View>
+    </ScreenWithMenu>
   );
 }
