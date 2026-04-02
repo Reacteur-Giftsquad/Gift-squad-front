d887e121b125a58642fa4bc1749dad11c6b58a99
Ewa Majowska <em.majowska@gmail.com>
2026-04-02 16:02:26 +0200

    Fix redirection after logout (menu and profil pages)

--- app/(main)/profil.js
+++ app/(main)/profil.js
@@ -12,8 +12,10 @@ import SubmitButton from "../../components/SubmitButton";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import ScreenWithMenu from "../../components/ScreenWithMenu";
+import { useRouter } from "expo-router";
 
 export default function Profil() {
+  const router = useRouter();
   const [isSubmitting, setIsSubmitting] = useState(false);
   const { user, updateUser, logout } = useAuth();
 
@@ -53,57 +55,60 @@ export default function Profil() {
   return (
     <ScreenWithMenu title="Mon profil">
       <View style={styles.content}>
-          <>
-            <View style={styles.center}>
-              <Octicons name="feed-person" size={100} color={colors.green} />
-              <Title
-                text={(user?.firstname || "") + " " + (user?.lastname || "")}
-                heading="h1"
-              />
-              <LightText text={`@${user?.pseudo || ""}`} />
-            </View>
-            <View style={styles.gap}>
-              <Title text="INFORMATION PERSONNELLES" heading="h2" />
-              <Input
-                title="Prénom"
-                placeholder={"Votre prénom"}
-                value={form.firstname}
-                setState={(v) => setForm({ ...form, firstname: v })}
-              />
-              <Input
-                title="Nom"
-                placeholder={"Votre nom"}
-                value={form.lastname}
-                setState={(v) => setForm({ ...form, lastname: v })}
-              />
-              <Input
-                title="Pseudo"
-                placeholder="Choisissez un pseudo unique"
-                value={form.pseudo}
-                setState={(v) => setForm({ ...form, pseudo: v })}
-              />
-              <Input
-                title="Email"
-                placeholder="Votre email"
-                setState={(v) => setForm({ ...form, email: v })}
-                value={form.email}
-              />
-            </View>
-            <View style={styles.gap}>
-              <SubmitButton
-                text="Enregistrer les modifications"
-                onPress={handleSubmit}
-                isSubmitting={isSubmitting}
-                icon={<Ionicons name="save" size={24} color="white" />}
-              />
-              <SubmitButton
-                text="Se déconnecter"
-                onPress={logout}
-                bgColor={colors.red}
-                icon={<MaterialIcons name="logout" size={24} color="white" />}
-              />
-            </View>
-          </>
+        <>
+          <View style={styles.center}>
+            <Octicons name="feed-person" size={100} color={colors.green} />
+            <Title
+              text={(user?.firstname || "") + " " + (user?.lastname || "")}
+              heading="h1"
+            />
+            <LightText text={`@${user?.pseudo || ""}`} />
+          </View>
+          <View style={styles.gap}>
+            <Title text="INFORMATION PERSONNELLES" heading="h2" />
+            <Input
+              title="Prénom"
+              placeholder={"Votre prénom"}
+              value={form.firstname}
+              setState={(v) => setForm({ ...form, firstname: v })}
+            />
+            <Input
+              title="Nom"
+              placeholder={"Votre nom"}
+              value={form.lastname}
+              setState={(v) => setForm({ ...form, lastname: v })}
+            />
+            <Input
+              title="Pseudo"
+              placeholder="Choisissez un pseudo unique"
+              value={form.pseudo}
+              setState={(v) => setForm({ ...form, pseudo: v })}
+            />
+            <Input
+              title="Email"
+              placeholder="Votre email"
+              setState={(v) => setForm({ ...form, email: v })}
+              value={form.email}
+            />
+          </View>
+          <View style={styles.gap}>
+            <SubmitButton
+              text="Enregistrer les modifications"
+              onPress={handleSubmit}
+              isSubmitting={isSubmitting}
+              icon={<Ionicons name="save" size={24} color="white" />}
+            />
+            <SubmitButton
+              text="Se déconnecter"
+              onPress={async () => {
+                await logout();
+                router.replace("(auth)/");
+              }}
+              bgColor={colors.red}
+              icon={<MaterialIcons name="logout" size={24} color="white" />}
+            />
+          </View>
+        </>
       </View>
     </ScreenWithMenu>
   );
