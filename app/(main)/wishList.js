6f853a5be9cba3ffdb12d3b5a15de147eb12eb2b
Ewa Majowska <em.majowska@gmail.com>
2026-04-08 13:46:23 +0400

    merge editGift and editWish

--- app/(main)/wishList.js
+++ app/(main)/wishList.js
@@ -66,7 +66,7 @@ export default function WishList() {
         onPress={() => {
           if (isMyList) {
             router.push({
-              pathname: "/(main)/editWish",
+              pathname: "/(main)/editGift",
               params: {
                 giftId: gift._id,
                 giftName: gift.name,
@@ -74,6 +74,7 @@ export default function WishList() {
                 giftLink: gift.link || "",
                 giftImage: gift.image_url || "",
                 giftDescription: gift.description || "",
+                mode: "wish",
               },
             });
           } else {
